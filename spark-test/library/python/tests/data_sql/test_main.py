2116900024f3409cb0a64b6bd50196f83c428861
JesseSchouten <jesse.schouten@outlook.com>
2022-01-29 10:21:12 +0100

    push latest version

--- spark-test/library/python/tests/data_sql/test_main.py
+++ spark-test/library/python/tests/data_sql/test_main.py
@@ -33,3 +33,20 @@ def test_dfs_count2():
     assert data.count() != 1
     spark.stop()
 
+def test_dfs_count2():
+    # Assumes that pyspark is installed on the machine!
+    # Suggestion: use databricks connect to use the databricks cluster
+    # https://docs.databricks.com/dev-tools/databricks-connect.html
+    columns = ['date', 'channel', 'country', 'nr_trials']
+    row = [('2017-12-15', 'hor', 'NL', 0)
+            ,('2018-12-15', 'hor', 'NL', 0)]
+
+    spark = SparkSession.builder.appName('SparkByExamples.com').getOrCreate()
+    rdd = spark.sparkContext.parallelize(row)
+
+    data = rdd.toDF(columns)   
+
+
+    assert data.count() == 2
+    spark.stop()
+
---
+++ spark-test/start_new.sh
@@ -0,0 +1,4 @@
+#!/bin/bash
+
+pip install /library/python
+pytest /library/python
