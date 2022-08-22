df1f92a1fa24f7aeefbc9b68871fc5c619a07e81
JesseSchouten <jesse.schouten@outlook.com>
2022-08-22 11:21:41 +0200

    feat: random feature that will not show up in changelog directly

--- pytest-test/library/python/src/lib/dc_azure_toolkit/blob.py
+++ pytest-test/library/python/src/lib/dc_azure_toolkit/blob.py
@@ -2,6 +2,8 @@ import sys
 
 from pyspark.sql import SparkSession
 
+
+
 def select_from_csv(storage_account_name, container_name, authentication_dict, file_path, delimiter =',', output_format='dfs', spark=None):
     """
     Gets data from a CSV file in a blob container on a storage account.
